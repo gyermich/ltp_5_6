25578a067de2f78a3550c4dac3e91c45ebac0837
Galina <yermichevagalina@gmail.com>
2013-10-29 21:33:13 -0700

    First Commit

---
+++ bigger_better_number.rb
@@ -0,0 +1,4 @@
+puts "Hello, what\'s your favorite number?"
+fav = gets.chomp
+num = fav.to_i + 1
+puts num.to_s + " is a bigger and better favorite number!"
---
+++ full_name_greeting.rb
@@ -0,0 +1,7 @@
+puts "Hello, what\'s your first name?"
+first_name = gets.chomp
+puts "What is your middle name?"
+middle_name = gets.chomp
+puts "And finally, what is you last name? "
+last_name = gets.chomp
+puts "Nice to meet you, " + first_name + " " + middle_name + " " + last_name + "!"
